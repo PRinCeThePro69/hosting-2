70c01dcfccecb105b48624434dfbaae7bd84edbc
PRinCeThePro69 <78784282+PRinCeThePro69@users.noreply.github.com>
2021-02-09 21:04:53 +0600

    4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const config = require('./config.json')
-const command = require('/command')
+const command = require('./command')
 
 client.once('ready', () => {
 	console.log('Ready!');
