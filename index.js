eb425877432d5449a84251b2ce6f501451cef18e
PRinCeThePro69 <78784282+PRinCeThePro69@users.noreply.github.com>
2021-02-09 21:03:19 +0600

    3

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
+const config = require('./config.json')
+const command = require('/command')
 
 client.once('ready', () => {
 	console.log('Ready!');
+
+	command(client, 'ping', message => {
+		message.channel.send(`Pong!`)
+	})
 });
+
+
+
 
 client.login(process.env.token)
