dc60db2251d15d3fab42d117c740902e0c1e0832
PRinCeThePro69 <78784282+PRinCeThePro69@users.noreply.github.com>
2021-02-11 22:33:46 +0600

    15

--- index.js
+++ index.js
@@ -47,16 +47,31 @@ client.once('ready', () => {
 	command (client, 'help', message => {
 		let embed = new Discord.MessageEmbed()
 		.setTitle('Help Menu')
-		.setDescription('My prefix is `a!`. The are the command list of me. **Make sure to use the prefix before each command!')
+		.setDescription('My prefix is `a!`. The are the command list of me. **Make sure to use the prefix before each command!**')
 		.setColor('RANDOM')
 		.addField('Miscellenious Commands(1)', "`help`")
 		.addField('Information Commands (1)', "`a!server`")
+		.setFooter('If you are a moderator in this server, then please use the "a!mod-cmds" command.')
+		.setTimestamp();
+		message.channel.send(embed);
 
-	 if (message.member.hasPermission('ADMINISTRATOR') || message.member.hasPermission('MANAGE_MESSAGES')) {
-		let membed = new Discord.MessageEmbed()
-		.addField('Moderation Commands(1)', "`purgeall`");
-		message.channel.send(membed, embed)
-	 }
+	// if (message.member.hasPermission('ADMINISTRATOR') || message.member.hasPermission('MANAGE_MESSAGES')) {
+		//let membed = new Discord.MessageEmbed()
+		//.addField('Moderation Commands(1)', "`purgeall`");
+		//message.channel.send(membed, embed)
+	// }
+	}) 
+	command(client, 'mod-cmds', message => {
+		var modperms  = (message.author.hasPermission('ADMINISTRATOR') || message.author.hasPermission('KICK_MEMBERS') || message.author.hasPermission('BAN_MEMBERS') || message.author.hasPermission('MANAGE_CHANNELS') || message.author.hasPermission('MANAGE_GUILD') || message.author.hasPermission('VIEW_AUDIT_LOG') || message.author.hasPermission('MANAGE_MESSAGES') || message.author.hasPermission('MENTION_EVERYONE') || message.author.hasPermission('VIEW_GUILD_INSIGHTS') || message.author.hasPermission('MUTE_MEMBERS') || message.author.hasPermission('DEAFEN_MEMBERS') || message.author.hasPermission('MOVE_MEMBERS') || message.author.hasPermission('MANAGE_NICKNAMES') || message.author.hasPermission('MANAGE_ROLES') || message.author.hasPermission('MANAGE_WEBHOOKS') || message.author.hasPermission('MANAGE_EMOJIS'))
+		
+		if(modperms) {
+			let membed = new Discord.MessageEmbed()
+			.setColor('RANDOM')
+			.addField('Moderation Commands(1)', "`purgeall`")
+			.setFooter('Want more help? user the "a!help" command!')
+		}
+		
+		
 	})
 });
 
